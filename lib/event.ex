74a2f4a2c4cdb28a0b147ab5b5e2ae58310ece1b
schurchill <schurchill@pang.esica.local>
2016-03-09 17:00:03 -0800

    Event module actually Does Something, and Something is What It's Supposed To Do.

--- lib/event.ex
+++ lib/event.ex
@@ -1,2 +1,15 @@
 defmodule Event do
+
+  require Record
+  Record.defrecord :state, [:server, :name, :to_go]
+
+  def loop(s) do
+    receive do
+      {server, ref, :cancel} -> send server, {ref, :ok}
+    after state(s, :to_go)*1000 ->
+      send state(s, :server), {:done, state(s, :name)}
+    end
+  end
+
 end
+
